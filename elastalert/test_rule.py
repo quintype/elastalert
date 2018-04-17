185d3c84b8f949dbe0b2af367460bf840abc2aa8
Utsav Kumar <utsav2307@gmail.com>
2018-04-17 14:43:22 +0530

    es version 6 fix

--- elastalert/test_rule.py
+++ elastalert/test_rule.py
@@ -54,7 +54,7 @@ class MockElastAlerter(object):
         es_client = elasticsearch_client(conf)
 
         try:
-            is_five = es_client.info()['version']['number'].startswith('5')
+            is_five = es_client.info()['version']['number'].startswith('6')
         except Exception as e:
             print("Error connecting to ElasticSearch:", file=sys.stderr)
             print(repr(e)[:2048], file=sys.stderr)
